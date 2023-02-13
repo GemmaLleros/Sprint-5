251ed9a0845fa167c5d74be55c4583f3aba379e5
GemmaLleros <gemma.caballero.serra@gmail.com>
2023-02-13 21:01:59 +0100

    Exercise 3

--- main.ts
+++ main.ts
@@ -1,29 +1,42 @@
 // 1. Se define una interface llamada "JokeResponse" con la propiedad llamada "joke". Esta interface describe el formato de la respuesta.
 interface JokeResponse {
-    joke: string;
+    joke : string;
 }
-// 2. Se define una función asíncrona que llama a una API de chistes para obtener un chiste aleatorio y mostrarlo en la página web.
-async function callRandomJoke() {
-// 2.1 Se establece la URL de la API.    
+interface Joke {
+    joke: string;
+    score: number;
+    date: string;
+}
+let reportJokes: Joke[] = [];
+
+async function callRandomJoke() {   
     const API_URL = 'https://icanhazdadjoke.com/';
-// 2.2 Se definen las opciones para la petición a la API, incluyendo header para indicar que se espera una respuesta en formato JSON.
-    const options: RequestInit = {
-        headers: {
-            'Accept': 'application/json'
-        }
-    };
-// 2.3 Se hace una petición a la API y se espera la respuesta. La respuesta se trata como un objeto de tipo JokeResponse.
+    const options: RequestInit = {headers: {'Accept': 'application/json'}};
     const jokeResponse = await (await fetch(API_URL, options)).json() as JokeResponse;
     console.log(jokeResponse);
+    const scoreButton = document.getElementById("scoreButtons") as HTMLButtonElement;
+    scoreButton.style.display = "";
+    
 // 2.4 Se obtiene un elemento HTML con el id "joke" y se le asigna el valor del chiste obtenido de la respuesta.
     const HTMLResponse = document.querySelector('#joke') as HTMLElement;
-
     HTMLResponse.innerHTML = jokeResponse.joke;
 }
-/*
-    3. Se agrega un event listener para el evento "DOMContentLoaded", que se ejecutará cuando el contenido del documento
-    haya sido cargado y parseado. Al dispararse este evento, se llama a la función "callRandomJoke".
-*/
-document.addEventListener("DOMContentLoaded", (event) => {
-    callRandomJoke();
-});
+
+function getPoints(id: number) {
+    const punctuation: number = id;
+    const textDate: string = new Date().toISOString();
+    const lastJokeElement = document.getElementById("joke");
+    let textLastJoke = "";
+    if (lastJokeElement) {
+      textLastJoke = lastJokeElement.outerText;
+    }
+    const jokeIndex = reportJokes.findIndex((e: Joke) => textLastJoke === e.joke);
+    const joke: Joke = { joke: textLastJoke, score: punctuation, date: textDate };
+  
+    if (textLastJoke !== "" && jokeIndex < 0) {
+      reportJokes.push(joke);
+    } else {
+      reportJokes[jokeIndex].score = punctuation;
+    }
+    console.log(reportJokes);
+  }
